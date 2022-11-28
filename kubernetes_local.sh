00717b4f498eacdc11c7d3d7e1d7e4bf4f00f260
Long <nve164@gmail.com>
2022-11-28 16:06:37 +0900

    Update kubernetes_local.sh

--- kubernetes_local.sh
+++ kubernetes_local.sh
@@ -19,26 +19,30 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # INSTALL KinD
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64
-chmod +x ./kind 
-sudo mv ./kind /usr/local/bin/
+install kind-linux-amd64 ~/.local/bin/kind
+kind create cluster --name kind-1
+
+# INSTALL k3d (k3s in docker)
+wget https://github.com/k3d-io/k3d/releases/download/v5.4.6/k3d-linux-amd64
+install k3d-linux-amd64 ~/.local/bin/k3d
+k3d cluster create -s3 -a3 test
 
 
 # INSTALL helm, kubens, kubectx, kubectl, minikube
 wget https://get.helm.sh/helm-v3.9.4-linux-amd64.tar.gz
 tar xzvf helm-v3.9.4-linux-amd64.tar.gz
-sudo mv ./linux-amd64/helm /usr/local/bin/
+install ./linux-amd64/helm ~/.local/bin/helm
 
 wget https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens
+install kubens ~/.local/bin/
 wget https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx
+install kubectx ~/.local/bin/
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install kubectl ~/.local/bin/
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install minikube-linux-amd64 ~/.local/bin/minikube
 
 # INSTALL cosign
 https://github.com/sigstore/cosign/releases/download/v1.13.1/cosign-linux-amd64
+install cosign-linux-amd64 ~/.local/bin/cosign
 
-# INSTALL k3d (k3s in docker)
-https://github.com/k3d-io/k3d/releases/download/v5.4.6/k3d-linux-amd64
-
-# TEST
-sudo kind create cluster --name kind-1
-sudo docker ps
